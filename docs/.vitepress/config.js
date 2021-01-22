f7e7ca625b667717e6fb5dbd62572df5ef5cab5e
君惜 <xinlei3166@126.com>
2021-01-22 11:32:15 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  title: 'VitePress',
+  description: 'Life is short, Keep it simple.',
+  head: [
+    [
+      'style',
+      {},
+      'img { border-radius: 10px }' + 'h1.title { margin-left: 0.5em }'
+    ]
+  ],
+  themeConfig: {
+    repo: 'xinlei3166/vitepress-demo',
+    docsDir: 'docs',
+    docsBranch: 'master',
+
+    // editLinks: true,
+    // editLinkText: 'Suggest changes to this page',
+    lastUpdated: 'Last Updated',
+
+    nav: [
+      { text: '文档', link: '/guide/' },
+      { text: 'API 参考', link: '/api/' },
+      { text: '插件', link: '/plugins/' },
+      {
+        text: '更新日志',
+        link:
+          'https://github.com/xinlei3166/vitepress-demo'
+      }
+    ],
+
+    sidebar: {
+      '/api/': 'auto',
+      '/plugins/': 'auto',
+      '/guide/': getGuideSidebar()
+    }
+  }
+}
+
+function getGuideSidebar() {
+  return [
+    {
+      text: '指南',
+      children: [
+        {
+          text: 'Git学习笔记1',
+          link: '/guide/Git学习笔记1'
+        },
+        {
+          text: 'Git学习笔记2',
+          link: '/guide/Git学习笔记2'
+        }
+      ]
+    },
+    {
+      text: '教程',
+      children: [
+        {
+          text: 'Git学习笔记1',
+          link: '/guide/Git学习笔记1'
+        },
+        {
+          text: 'Git学习笔记2',
+          link: '/guide/Git学习笔记2'
+        }
+      ]
+    }
+  ]
+}
