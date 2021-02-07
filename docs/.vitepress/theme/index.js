4a3f622e4a5d09a917121288bb920c1446356ba6
君惜 <xinlei3166@126.com>
2021-02-07 20:26:31 +0800

    chore: deploy to gh-pages.

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,5 @@
 import theme from 'vitepress/dist/client/theme-default'
-import Demo from '../dynamic/demo.vue'
+// import Demo from '../dynamic/demo.vue'
 import Button from '../../../src/components/Button.vue'
 import '../../../src/styles/index.css'
 import { registerComponents } from './register-components'
@@ -15,7 +15,7 @@ export default {
     // router is VitePress' custom router (see `lib/app/router.js`)
     // siteData is a ref of current site-level metadata.
     app.component(Button.name, Button)
-    app.component(Demo.name, Demo)
+    // app.component(Demo.name, Demo)
     registerComponents(app)
   }
 }
