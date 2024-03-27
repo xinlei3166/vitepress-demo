4fd42fd2f81042f3dbcace58d64af30de3c75291
君惜 <xinlei3166@126.com>
2024-03-27 17:28:03 +0800

    chore: deploy

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -3,11 +3,14 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 查看当前目录
+pwd
+
 # 生成静态文件
-yarn run docs:build
+pnpm run docs:build
 
 # 进入生成的文件夹
-cd ../docs/.vitepress/dist
+cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
